cb380d793014d103b37e20961f901004aa28ac0f
Taufik-Mulla <mullataufik0313@gmail.com>
2025-12-09 23:43:27 +0530

    updated

---
+++ src/test/java/com/jbk/Login.java
@@ -0,0 +1,26 @@
+package com.jbk;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test
+	void login1() {
+		System.out.println("Home1");
+
+	}
+
+	@Test
+	void login2() {
+		System.out.println("Home2");
+
+	}
+
+	@Test
+	void login3() {
+		System.out.println("Home3");
+		{
+
+		}
+
+	}
+}
