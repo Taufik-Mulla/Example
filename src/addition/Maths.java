12c8773c70be41829031c32473caa04c7d91a5ce
Taufik-Mulla <mullataufik0313@gmail.com>
2025-12-12 22:40:51 +0530

    First commit

---
+++ src/addition/Maths.java
@@ -0,0 +1,24 @@
+package addition;
+
+public class Maths {
+ public static void main(String[] args) {
+	 
+	
+	 int a=23;
+	 int b=56;
+	 System.out.println(a+b);
+	 
+	 int a1=23;
+	 int b1=56;
+	 System.out.println(a-b);
+
+	 int a2=23;
+	 int b2=56;
+	 System.out.println(a*b);
+
+	 int a3=23;
+	 int b4=56;
+	 System.out.println(a/b);
+
+}
+}
